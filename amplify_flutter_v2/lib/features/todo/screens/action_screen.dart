54d4af6950c3d6e9769ffff8ea37ecf503fa1020
Hikmatullah <hikmathakim7200@gmail.com>
2025-09-11 14:59:55 +0100

    i added some animation to auto change to next timer

--- amplify_flutter_v2/lib/features/todo/screens/action_screen.dart
+++ amplify_flutter_v2/lib/features/todo/screens/action_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:ember/features/todo/widget/pomodoro_todo.dart';
 
 import 'package:flutter/material.dart';
@@ -10,15 +12,58 @@ class ActionScreen extends StatefulWidget {
 }
 
 class _ActionScreenState extends State<ActionScreen> {
+  int? selectedIndex;
   // Move tasks to be a state variable
   final List<Map<String, dynamic>> tasks = [
-    {"title": "Read the book and ", "isCompleted": true, "timeElapsed": 20},
-    {"title": "Watch the video", "isCompleted": false, "timeElapsed": 0},
-    {"title": "Write the summary", "isCompleted": false, "timeElapsed": 0},
-    {"title": "Review the notes", "isCompleted": true, "timeElapsed": 4},
-    {"title": "Review the video", "isCompleted": false, "timeElapsed": 14},
-    {"title": "Review the book", "isCompleted": true, "timeElapsed": 20},
+    {"title": "Read the book and ", "timeElapsed": 0, "type": "pomodoro"},
+    {"title": "Short break", "timeElapsed": 0, "type": "short_break"},
+    {"title": "Watch the video", "timeElapsed": 0, "type": "pomodoro"},
+    {"title": "Short break", "timeElapsed": 0, "type": "short_break"},
+    {"title": "Write the summary", "timeElapsed": 0, "type": "pomodoro"},
+    {"title": "Short break", "timeElapsed": 0, "type": "short_break"},
+    {"title": "Review the notes", "timeElapsed": 0, "type": "pomodoro"},
+    {"title": "Long break", "timeElapsed": 0, "type": "long_break"},
+    {"title": "Review the video", "timeElapsed": 0, "type": "pomodoro"},
+    {"title": "Long break", "timeElapsed": 0, "type": "long_break"},
+    {"title": "Review the book", "timeElapsed": 0, "type": "pomodoro"},
   ];
+
+  Timer? _timer;
+  int duration = 25;
+
+  void _startTimer(int index) {
+    _timer?.cancel(); // stops the timer
+
+    // Reset the current task's timer if switching to a different task
+    if (selectedIndex != null && selectedIndex != index) {
+      setState(() {
+        tasks[selectedIndex!]['timeElapsed'] = 0;
+      });
+    }
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      //code runs every second
+      if (tasks[index]['timeElapsed'] < duration) {
+        //incriments until time is 25
+        setState(() {
+          tasks[index]['timeElapsed'] = tasks[index]['timeElapsed'] + 1;
+        });
+      } else {
+        if (tasks.length > index) {
+          index++;
+          setState(() {
+            selectedIndex = index;
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,17 +76,20 @@ class _ActionScreenState extends State<ActionScreen> {
             child: ListView.builder(
               itemCount: tasks.length,
               itemBuilder: (BuildContext context, int index) {
+                bool isSelected = selectedIndex == index;
+
                 return PomodoroTodo(
+                  isSelected: isSelected,
                   title: tasks[index]['title'],
-                  currentProgress: tasks[index]['timeElapsed'],
-                  totalProgress: 20,
+                  currentTime: tasks[index]['timeElapsed'],
+                  maxTime: 25,
                   onTap: () {
                     setState(() {
-                      print("tapp exectuted");
-                      tasks[index]['timeElapsed'] =
-                          tasks[index]['timeElapsed'] + 1;
+                      selectedIndex = index;
                     });
+                    _startTimer(index);
                   },
+                  type: tasks[index]['type'],
                 );
               },
             ),
--- amplify_flutter_v2/lib/features/todo/widget/pomodoro_todo.dart
+++ amplify_flutter_v2/lib/features/todo/widget/pomodoro_todo.dart
@@ -1,34 +1,46 @@
+import 'package:ember/app/theme.dart';
 import 'package:ember/features/todo/widget/progress_tomato.dart';
 import 'package:flutter/material.dart';
 
 class PomodoroTodo extends StatelessWidget {
   final String title;
 
-  final int currentProgress;
-  final int totalProgress;
+  final int currentTime;
+  final int maxTime;
   final VoidCallback onTap;
+  final bool isSelected;
+  final String type;
 
   const PomodoroTodo({
     super.key,
     required this.title,
-
-    required this.currentProgress,
-    required this.totalProgress,
+    required this.currentTime,
+    required this.maxTime,
     required this.onTap,
+    required this.isSelected,
+    required this.type,
   });
 
   @override
   Widget build(BuildContext context) {
-    final double percentage = totalProgress > 0
-        ? currentProgress / totalProgress
-        : 0.0;
+    final double percentage = maxTime > 0 ? currentTime / maxTime : 0.0;
 
     return GestureDetector(
       onTap: onTap,
       child: Container(
         margin: const EdgeInsets.only(bottom: 8),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: isSelected ? AppColors.pomodoroRed : Colors.transparent,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
         child: Material(
-          color: Colors.white,
+          color: type == "pomodoro"
+              ? Colors.white
+              : type == "long_break"
+              ? const Color.fromARGB(255, 50, 255, 211)
+              : const Color.fromARGB(255, 206, 255, 181),
           borderRadius: BorderRadius.circular(12),
           elevation: 2,
           shadowColor: Colors.grey.withAlpha(26),
